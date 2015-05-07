27bd6f4d3c2425bb1da8c53c0cee96e3bbc9ec82
Billy1339 <william.m.sargent@gmail.com>
2015-05-06 23:35:42 -0400

    put requests when voting occurs

--- js.js
+++ js.js
@@ -3,47 +3,75 @@
       cvalue = true;
       var d = new Date();
       // d.setTime(d.getTime() + (1*24*60*60*1000));
-      d.setTime(d.getTime() + (10000));
+      d.setTime(d.getTime() + (30000));
       var expires = "expires="+d.toUTCString();
       document.cookie = cname + "=" + cvalue + "; " + expires;
   }
-
 
   function getCookie() {
       var x = document.cookie === "www.vote-hillary-2016.com=true"
       return x
   }
 
-  function checkForCookie() {
-    var x = getCookie();
-    if (x !== true) {
-      setCookie();
-      // alert('==! worked');
+  function addPts(element) {
+    var id, x, y;
+    id = element.id;
+    x = parseInt($('#'+id+"Add").html());
+    y = x + 1;
+    $('#'+id+"Add").html(y);
+    if (id === "thumbsUp") {
+      requestUp(y);
+    } else {
+      requestDown(y);
     }
   };
 
-  function addPts() {
-    // debugger
+  function addAVote(element) {
+    var y = getCookie();
+    if (y === true) {
+      alert("It looks like you have already voted. We are doing our very best to insure an accurate count on these votes and do not want voters to repeat. Please have you friends vote and check back in to see where the vote is at.")
+    } else {
+      setCookie();
+      addPts(element);
+    }
   };
 
 
+
+  function requestUp(newNumber) {
+    $.ajax({
+        url: 'http://localhost:3000/thumbs_ups/1',
+        type: 'PUT',
+        data: {thumbs_up: {
+          voteNumber: newNumber }
+        }
+    }).done(function(response) {
+      debugger
+      console.log(response);
+    });
+  }
+
+  function requestDown(newNumber) {
+    $.ajax({
+        url: 'http://localhost:3000/thumbs_downs/1',
+        type: 'PUT',
+        data: {thumbs_down: {
+          voteNumber: newNumber }
+        }
+    }).done(function(response) {
+      debugger
+      console.log(response);
+    });
+  }
 
 
 $(document).ready(function() {
 
-  checkForCookie();
-  $('#something').on('click', function() {
-    var x = getCookie();
-    console.log(x);
-  });
-
   $('#thumbsUp').on('click', function() {
-    var x = parseInt($('#'+this.id+"Add").html());
-    $('#'+this.id+"Add").html(x+1);
+    addAVote(this);
   });
   $('#thumbsDown').on('click', function() {
-    var x = parseInt($('#'+this.id+"Add").html());
-    $('#'+this.id+"Add").html(x+1);
+    addAVote(this);
   });
 
 });
